a10870fea397bce27f038b7ad0941da77160c615
Steve Bagnall <steve.bagnall@gmail.com>
2019-05-06 10:40:08 +0100

    added facebook long-lived (offline) token

--- server.js
+++ server.js
@@ -14,18 +14,24 @@ app.use('/oauthCreds.json', (req, res) => {
     "client_id": process.env.CLIENT_ID,
     "auth_uri": "https://accounts.google.com/o/oauth2/v2/auth",
     "token_uri": "https://www.googleapis.com/oauth2/v4/token",
-    "client_secret": process.env.CLIENT_SECRET
+    "client_secret": process.env.CLIENT_SECRET,
+    "app_id": process.env.APP_ID,
+    "facebook_auth_uri": "https://www.facebook.com/v3.3/dialog/oauth",
+    "facebook_token_uri": "https://graph.facebook.com/v3.3/oauth/access_token",
+    "app_secret": process.env.APP_SECRET
   }));
+});
+
+app.use('/redirect', (req, res) => {
+  res.redirect(`/?type=youtube&code=${req.query.code}&scope=${req.query.scope}`);
+});
+
+app.use('/fbredirect', (req, res) => {
+  res.redirect(`/?type=facebook&code=${req.query.code}`);
 });
 
 app.use('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname });
-})
-
-app.use('/redirect', (req, res) => {
-  console.info(req.query.code);
-  console.info(req.query.scope);
-  res.redirect(`/?code=${req.query.code}&scope=${req.query.scope}`);
 });
 
 app.listen(5000, () => console.log('Listening on port 5000'))
